ed2e75d9672d2584d08836082821215b60988815
bburke02 <brendenburke2@gmail.com>
2021-09-08 23:10:07 -0400

    updated about me

--- src/components/App.js
+++ src/components/App.js
@@ -37,7 +37,7 @@ const dogs = [
     name: "Scotty",
     breed: "Havanese",
     aboutMe:
-      "My name is Scotty and I am three years old. I like to play with tennis balls, play tug, and wrestle with my friends. I have hip dysplasia, so sometimes it is uncomfortable when I run.",
+      "My name is Scotty and I am three years old. I like to play with tennis balls, play tug, and wrestle with my friends. I have hip dysplasia, so sometimes it is uncomfortable when I run. Take a look at the map to see where I am originally from!",
     profilePic: scottyProfile,
     age: 3,
     coordinates: {
@@ -50,7 +50,7 @@ const dogs = [
     name: "Daisy",
     breed: "American Foxhound/Dauschund",
     aboutMe:
-      "My name is Daisy and I am two years old. I was rescued by my human parents from Alabama, along with my sister Rosie. I like to eat anything and everything. I  beg for food when I am not supposed to. I am faster than my brother Scotty, but I always let him catch me when we are playing tag in the backyard.",
+      "My name is Daisy and I am two years old. I was rescued by my human parents from Alabama, along with my sister Rosie. I like to eat anything and everything. I  beg for food when I am not supposed to. I am faster than my brother Scotty, but I always let him catch me when we are playing tag in the backyard. Take a look at the map to see where I am originally from!",
     profilePic: daisyProfile,
     age: 2,
     coordinates: {
@@ -64,7 +64,7 @@ const dogs = [
     name: "Rosie",
     breed: "American Foxhound/Dauschund",
     aboutMe:
-      "My name is Rosie and I am two years old. I was rescued by my human mommy and daddy from Alabama, along with my sister, Daisy. I love to destroy toys even though my mommy doesn't like that. I like to play with sister Daisy and my best friend Scotty. We usually get together once a week for a long walk on the street and some chasing and wrestling outside!",
+      "My name is Rosie and I am two years old. I was rescued by my human mommy and daddy from Alabama, along with my sister, Daisy. I love to destroy toys even though my mommy doesn't like that. I like to play with sister Daisy and my best friend Scotty. We usually get together once a week for a long walk on the street and some chasing and wrestling outside! Take a look at the map to see where I am originally from!",
     profilePic: rosieProfile,
     age: 2,
     coordinates: {
@@ -77,7 +77,7 @@ const dogs = [
     name: "Sammy",
     breed: "Beagle",
     aboutMe:
-      "My name is Sammy and I don't even know how old I am. I was rescued by my human mommies from a shelter. I love to eat. I howl when I want to have food. I get sad when my parents leave me, and get overcome with emotion when they come home. I don't like to play too much, but when I do I get a burst of energy for 10 seconds",
+      "My name is Sammy and I don't even know how old I am. I was rescued by my human mommies from a shelter. I love to eat. I howl when I want to have food. I get sad when my parents leave me, and get overcome with emotion when they come home. I don't like to play too much, but when I do I get a burst of energy for 10 seconds Take a look at the map to see where I am originally from!",
     profilePic: sammyProfile,
     age: 7,
     coordinates: {
